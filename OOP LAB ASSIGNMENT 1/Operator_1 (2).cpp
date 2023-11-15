a752f90123e15ecfaab709986b9041ffa6ba08ca
M. ABRAR UL HAQUE <91060008+MAbrarulhaq@users.noreply.github.com>
2023-11-15 23:25:47 +0500

    Add files via upload

---
+++ OOP LAB ASSIGNMENT 1/Operator_1 (2).cpp
@@ -0,0 +1,18 @@
+// Name: M.Abrar ul haque
+// Roll No: P21 8038
+#include <iostream>
+
+using namespace std;
+int main(){
+	
+	int rupees,dollars;
+	cout<<"enter the amount of dollars: ";
+	cin>>dollars;
+	
+	
+	rupees= dollars*228.48; //price of 1$ into rupees
+	
+	cout<<"the amount in rupees is: "<<rupees;
+	
+	return 0;
+}
---
+++ OOP LAB ASSIGNMENT 1/Operator_2(2).cpp
@@ -0,0 +1,20 @@
+// Name: M.Abrar ul haque
+// Roll No: P21 8038
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int rupees;
+	cout<<"enter the amount of rupees: ";
+	cin>>rupees;
+	
+	float dollars;
+	dollars= (float)rupees/228.48; //price of 1$ into rupees
+	
+	cout<<"the amount in dollars is: "<<dollars;
+	
+	return 0;
+	
+}
